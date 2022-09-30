85d1dedec05adf146e3391e0641fe53d7fc4ed10
henriquecfaguiar <henriquecfaguiar@gmail.com>
2022-09-30 12:10:47 -0300

    Add hover effect

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid");
+      container.appendChild(grid);
+    };
   };
-}
+};
+
+function addHover() {
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach(element => {
+    element.addEventListener("mouseover", (element) => { console.log(element) });
+  });
+};
+
+createGrid();
