8b1b77b6985b0e20e9f36798d2ebbf8508368754
henriquecfaguiar <henriquecfaguiar@gmail.com>
2022-10-04 17:24:46 -0300

    Layout adjustments and start to add responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const root = document.documentElement;
 
-const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 const newGridBtn = document.querySelector(".new-grid-btn");
 const clearBtn = document.querySelector(".clear-btn");
 
@@ -13,29 +13,33 @@ function createGrid(gridSize) {
   root.style.setProperty("--grid-size", gridSize);
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
-      const grid = document.createElement("div");
-      grid.classList.add("grid");
-      container.appendChild(grid);
+      const gridElement = document.createElement("div");
+      gridElement.classList.add("grid-element");
+      grid.appendChild(gridElement);
     };
   };
   addHover();
 };
 
 function getNewGridSize() {
-  let gridSize = Number(prompt("Choose a new grid size:"));
+  let gridSize = Number(prompt("Choose a new grid size (max: 100)"));
+  console.log(gridSize);
+  if (gridSize === 0) {
+    gridSize = 16;
+  };
   clearGrid();
   createGrid(gridSize);
 };
 
 function addHover() {
-  let grid = document.querySelectorAll(".grid");
+  let grid = document.querySelectorAll(".grid-element");
   grid.forEach(element => {
     element.addEventListener("mouseover", (element) => { element.target.classList.add("hover") });
   });
 };
 
 function clearGrid() {
-  let grid = document.querySelectorAll(".grid");
+  let grid = document.querySelectorAll(".grid-element");
   grid.forEach((element) => element.classList.remove("hover"));
 };
 
